5cfe81facd57064591f0cbfb6a18a1f993fed23e
Rin1ton <n00i3.n4.i.0reooo@gmail.com>
2024-07-09 10:06:42 -0400

    Successfully breaks salesforce

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,28 @@
-const article = document.querySelector("p");
+//create new element
+var newTestElement = document.createElement("h2");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+var newTextNode = document.createTextNode("😎 COOL beginning of row BEANS 😎");
+//add text to new element
+newTestElement.appendChild(newTextNode);
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+var div=document.createElement("div");
+document.body.appendChild(div);
+div.innerText="😎 COOL end of body BEANS 😎";
 
-  article.insertAdjacentElement("p", badge);
+var listOfSpan = document.querySelectorAll("span");
+
+listOfSpan.forEach(element => {
+  var coolestBeans = document.createElement("p");
+  coolestBeans.appendChild(document.createTextNode("😎 COOL every span tag BEANS 😎"));
+  element.insertAdjacentElement("beforebegin", coolestBeans);
+});
+
+var myVeryCoolStatus = document.getElementsByClassName("slds-truncate unavailable-code-state agent-state available-state-color");
+myVeryCoolStatus.insertAdjacentElement("beforeend", newTestElement);
+
+//append the element to another element
+if (document.getElementsByClassName("row") != null) {
+  document.getElementsByClassName("row")[0].insertAdjacentElement("afterbegin", newTestElement);
 }
 
 /*const article = document.querySelector("article");
